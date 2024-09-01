4f65f986182dfdecf83c42ee7bdbbd11e76e6fd1
Marek Skopal <skopal.marek@gmail.com>
2024-09-01 20:36:59 +0200

    Fix missing get methods

--- src/Trading212.php
+++ src/Trading212.php
@@ -4,7 +4,12 @@ declare(strict_types=1);
 
 namespace MarekSkopal\Trading212;
 
+use MarekSkopal\Trading212\Api\AccountData;
+use MarekSkopal\Trading212\Api\EquityOrders;
+use MarekSkopal\Trading212\Api\HistoricalItems;
 use MarekSkopal\Trading212\Api\InstrumentsMetadata;
+use MarekSkopal\Trading212\Api\PersonalPortfolio;
+use MarekSkopal\Trading212\Api\Pies;
 use MarekSkopal\Trading212\Client\Client;
 use MarekSkopal\Trading212\Config\Config;
 
@@ -13,6 +18,11 @@ class Trading212
     private readonly Client $client;
 
     private InstrumentsMetadata $instrumentsMetadata;
+    private Pies $pies;
+    private EquityOrders $equityOrders;
+    private AccountData $accountData;
+    private PersonalPortfolio $personalPortfolio;
+    private HistoricalItems $historicalItems;
 
     public function __construct(Config $config)
     {
@@ -28,5 +38,60 @@ class Trading212
         $this->instrumentsMetadata = new InstrumentsMetadata($this->client);
 
         return $this->instrumentsMetadata;
+    }
+
+    private function getPies(): Pies
+    {
+        if (isset($this->pies)) {
+            return $this->pies;
+        }
+
+        $this->pies = new Pies($this->client);
+
+        return $this->pies;
+    }
+
+    private function getEquityOrders(): EquityOrders
+    {
+        if (isset($this->equityOrders)) {
+            return $this->equityOrders;
+        }
+
+        $this->equityOrders = new EquityOrders($this->client);
+
+        return $this->equityOrders;
+    }
+
+    private function getAccountData(): AccountData
+    {
+        if (isset($this->accountData)) {
+            return $this->accountData;
+        }
+
+        $this->accountData = new AccountData($this->client);
+
+        return $this->accountData;
+    }
+
+    private function getPersonalPortfolio(): PersonalPortfolio
+    {
+        if (isset($this->personalPortfolio)) {
+            return $this->personalPortfolio;
+        }
+
+        $this->personalPortfolio = new PersonalPortfolio($this->client);
+
+        return $this->personalPortfolio;
+    }
+
+    private function getHistoricalItems(): HistoricalItems
+    {
+        if (isset($this->historicalItems)) {
+            return $this->historicalItems;
+        }
+
+        $this->historicalItems = new HistoricalItems($this->client);
+
+        return $this->historicalItems;
     }
 }
